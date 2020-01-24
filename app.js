d0cbd6582c0be44d761415ee9931a85ae42915cc
cihan Çakır <cihancakir@trakya.edu.tr>
2020-01-24 13:05:02 +0300

    End Movie

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var movie =  require('./routes/movie');
+var movie = require('./routes/movie');
 
 var app = express();
 /** 
@@ -19,7 +19,7 @@ mongoose.connect('mongodb://localhost/movieapi', { useMongoClient: true})
   console.log('MongoDB: Error',err);
  });
 */
- // DB connection
+// DB connection
 const db = require('./db/Connection')();
 
 
@@ -39,19 +39,19 @@ app.use('/users', usersRouter);
 app.use('/api/movie', movie);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error: { message: err.message, status: err.code }, status: err.status });
 });
 
 module.exports = app;
--- routes/movie.js
+++ routes/movie.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const db = require('mongoose')
 // Movie Model Entity
 const Movie = require('../models/Movie');
 
 
+// [POST] NEW MOVİE 
 router.post('/new', (req, res, next) => {
 
     const movie = new Movie(req.body);
@@ -27,9 +29,117 @@ router.post('/new', (req, res, next) => {
   */
 });
 
+// [GET] MOVİE LİST
+router.get('/', (req, res, next) => {
+    const promise = Movie.find({});
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+    /*
+    Movie.find({}, (err, data) => {
+        res.json(data);
+    });
+    */
+});
+
+// [UPDATE] Movie
+
+router.put('/:movie_id', (req, res, next) => {
+    const promise = Movie.findByIdAndUpdate(
+        req.params.movie_id,
+        req.body,
+        {
+            new: true
+        }
+    );
+    promise.then((data) => {
+        if (!data)
+            next({ message: 'This movie not found', code: res.statusCode });
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+})
+
+// [DELETE] Movie
+router.delete('/:movie_id', (req, res, next) => {
+    const promise = Movie.findByIdAndDelete(req.params.movie_id);
+    promise.then((data) => {
+        if (!data)
+            next({ message: 'This movie not found', code: res.statusCode });
+        res.json({ message: 'This movie succesfull delete', code: res.statusCode });
+    }).catch((err) => {
+        res.json(err);
+    });
+})
+
+// [GET] MOVİE İNFO BY ONE 
+router.get('/:movie_id', (req, res, next) => {
+    const promise = Movie.findById(
+        req.params.movie_id,
+    );
+    promise.then((data) => {
+        if (!data)
+            next({ message: 'This movie not found', code: res.statusCode });
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+// [Get] Top :Value  list by tip
+router.get('/top/:value', (req, res, next) => {
+    const promise = Movie.aggregate([
+        {
+            $sort: {
+                imdb_score: -1
+            }
+        },
+        {
+            $limit: Number(req.params.value)
+        }
+    ]);
+
+    const moviecount = Movie.find({}).count()
+
+    promise.then((data) => {
+        if (Number(req.params.value) > moviecount)
+            next({ message: 'we havent yet count ' + moviecount + ' ' + req.params.value + ' of movie', code: res.statusCode });
+        if (!data)
+            next({ message: 'This movie not found', code: res.statusCode });
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+})
+
+// [GET] Between  Date
+
+router.get('/between/:start_year/:end_year', (req, res) => {
+    // $gte büyük ve eşit demektir  küçük veya eşit demektir
+    const { start_year, end_year } = req.params;
+    const promise = Movie.find(
+        {
+            year: { "$gte": parseInt(start_year), "$lte": parseInt(end_year) }
+        }
+    );
+    promise.then((data) => {
+        if (!data)
+            next({ message: 'This movie not found', code: res.statusCode });
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+
+});
+
+
 
 router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
+
 
 module.exports = router;
