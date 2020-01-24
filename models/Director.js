d59adfaa86cb6320733b076626139fb1253cca43
cihan Çakır <cihancakir@trakya.edu.tr>
2020-01-24 16:06:49 +0300

    validation complete

--- models/Director.js
+++ models/Director.js
@@ -4,15 +4,22 @@ const Schema = mongoose.Schema;
 const DirectorSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        maxlength: [15, '`{PATH} alanı (`{VALUE}`), ({MAXLENGTH}) karakterden KÜÇÜK olmalıdır'],
+        minlength: [1, '`{PATH} alanı (`{VALUE}`), ({MINLENGTH}) karakterden BÜYÜK olmalıdır'],
     },
     surname: {
         type: String,
-        required: true
+        required: true,
+        maxlength: [15, '`{PATH} alanı (`{VALUE}`), ({MAXLENGTH}) karakterden KÜÇÜK olmalıdır'],
+        minlength: [1, '`{PATH} alanı (`{VALUE}`), ({MINLENGTH}) karakterden BÜYÜK olmalıdır'],
+
     },
     age: {
         type: Number,
-        required: true
+        required: true,
+        maxlength: [10, '`{PATH} alanı (`{VALUE}`), ({MAXLENGTH}) karakterden KÜÇÜK olmalıdır'],
+        minlength: [100, '`{PATH} alanı (`{VALUE}`), ({MINLENGTH}) karakterden BÜYÜK olmalıdır'],
     },
     createdAt: {
         type: Date,
--- models/Movie.js
+++ models/Movie.js
@@ -1,19 +1,22 @@
 const moongose = require('mongoose')
 const Schema = moongose.Schema
 const MovieSchema = new Schema({
-    director_id : Schema.Types.ObjectId,
+    director_id: Schema.Types.ObjectId,
     title: {
         type: String,
-        require: true,
-        unique: true
+        require: [true, '`{PATH}` alanı zorunludur '],
+        unique: [true, '`{PATH}` alanı benzersiz olmalıdır'],
+        maxlength: [15, '`{PATH} alanı (`{VALUE}`), ({MAXLENGTH}) karakterden KÜÇÜK olmalıdır'],
+        minlength: [4, '`{PATH} alanı (`{VALUE}`), ({MINLENGTH}) karakterden BÜYÜK olmalıdır'],
+
     },
     category: String,
     country: String,
     imdb_score: Number,
     year: Number,
-    date : {
+    date: {
         type: Date,
-        default : Date.now  
+        default: Date.now
     }
 });
 
